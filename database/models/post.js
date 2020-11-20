ac1857ecd3baf780eaa6f7d98ddc976807d57ee5
mani3354488 <mani3354488@gmail.com>
2020-11-20 14:47:46 +0530

    migrating db

--- database/models/post.js
+++ database/models/post.js
@@ -1,25 +1,17 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Post extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+  const Post = sequelize.define(
+    'Post',
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.TEXT,
+      userId: DataTypes.INTEGER,
+    },
+    {},
+  );
+  Post.associate = function (models) {
+    // associations can be defined here
+    Post.belongsTo(models.User, { foreignKey: 'userId', as: 'author' });
+    Post.hasMany(models.Comment, { foreignKey: 'postId', as: 'comments' });
   };
-  Post.init({
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT,
-    userId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Post',
-  });
   return Post;
 };
--- database/models/user.js
+++ database/models/user.js
@@ -1,25 +1,31 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      defaultScope: {
+        rawAttributes: { exclude: ['password'] },
+      },
+    },
+  );
+
+  User.beforeCreate(async (user) => {
+    user.password = await user.generatePasswordHash();
+  });
+  User.prototype.generatePasswordHash = function () {
+    if (this.password) {
+      return bcrypt.hash(this.password, 10);
     }
   };
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.associate = function (models) {
+    // associations can be defined here
+    User.hasMany(models.Post, { foreignKey: 'userId', as: 'posts' });
+  };
   return User;
 };
