48c354c44ac70f3c0fd0c52fca1fd7fb93710079
mani3354488 <mani3354488@gmail.com>
2020-11-20 15:31:50 +0530

    saving all files

--- database/config/config.js
+++ database/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: 'devdatabase',
     host: '127.0.0.1',
     dialect: 'postgres',
-    use_env_variable: 'postgres://postgres:postgres@localhost:5432/devdatabase',
+    use_env_variable: 'DATABASE_URL',
   },
   test: {
     username: 'root',
@@ -17,7 +17,7 @@ module.exports = {
     dialect: 'postgres',
     host: '127.0.0.1',
     dialect: 'postgres',
-    use_env_variable: '',
+    use_env_variable: 'postgresql://postgres:postgres@localhost:5432/database_test',
   },
   production: {
     username: 'root',
