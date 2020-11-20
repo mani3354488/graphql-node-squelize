9632fc52883c4cd7e777cd4e9ead174dcd679034
mani3354488 <mani3354488@gmail.com>
2020-11-20 13:48:06 +0530

    starting server

---
+++ graphql/schemas/index.js
@@ -0,0 +1,11 @@
+cosnt { gql } = require('apollo-server-express');
+
+const rootType = gql`
+type Query {
+    root: String
+}
+type Mutation {
+    root: String
+}`;
+
+module.exports = [rootType];
